15b72f71204591d26aec9cfce33d3686f9f1c802
Jacob Baldwin <baldwin.jacob1997@gmail.com>
2019-02-11 21:33:29 -0700

    Switching back to inclusion of hash routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ const appRoutes: Routes = [
     ConnectComponent
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     BrowserModule,
     ScrollDispatchModule
   ],
